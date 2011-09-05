b5213896af252272a68f983beafebc4b16f0dd91
Sam Ward <sams@live.co.uk>
2011-09-05 17:30:49 +0100

    Moved js to script.js

--- js/script.js
+++ js/script.js
@@ -1,48 +1,51 @@
+(function ($) {
 
-$(document).ready(function ($) {
-  // put all your jQuery goodness in here.
+    Todo = Backbone.Model.extend({
+        //Create a model to hold todo atribute
+        defaults: {
+            name: 'Unnamed Task',
+            done: false
+        },
+    });
 
-	Todo = Backbone.Model.extend({
-    //Create a model to hold friend atribute
-    name: null,
-	done: false
-  });
+    Todos = Backbone.Collection.extend({
+        //This is our Todos collection and holds our Todo models
+        initialize: function (models, options) {
+            this.bind("add", options.view.addTodoLi);
+            //Listen for new additions to the collection and call a view function if so
+        }
+    });
 
-  Todos = Backbone.Collection.extend({
-    //This is our Friends collection and holds our Friend models
-    initialize: function (models, options) {
-      this.bind("add", options.view.addTodoLi);
-      //Listen for new additions to the collection and call a view function if so
-    }
-  });
+    AppView = Backbone.View.extend({
+        el: $("body"),
+        initialize: function () {
+            this.todos = new Todos(null, {
+                view: this
+            });
+            //Create a todos collection when the view is initialized.
+            //Pass it a reference to this view to create a connection between the two
+        },
+        events: {
+            "click #addTask": "getInput",
+        },
+        getInput: function () {
+            var todo_name = $('#taskInput').val();
+			var todo_model = null;
+			
+			if (todo_name == '')
+            	todo_model = new Todo();	
+			else 
+				todo_model = new Todo({ name: todo_name })
+				
+            //Add a new todo model to our todo collection
+            this.todos.add(todo_model);
+        },
+        addTodoLi: function (model) {
+            //The parameter passed is a reference to the model that was added
+            $("#todo-list").append("<a href='#'><li>" + model.get('name') + "</li></a>");
+            //Use .get to receive attributes of the model
+        }
+    });
 
-  AppView = Backbone.View.extend({
-    el: $(".content"),
-    initialize: function () {
-      this.todos = new Todos( null, { view: this });
-    },
-    events: {
-      "click #addTask":  "addTodoLi",
-    },
-    addTodoLi: function (model) {
-      //The parameter passed is a reference to the model that was added
-	  var todo_model = new Todo({name: 'x'});
-	  this.Todos.add( todo_model );
-      $("#todo-list").append("<li>" + model.get('name') + "</li>");
-      //Use .get to receive attributes of the model
-    }
-  });
-
-  var appview = new AppView;
- 
- 	return false;
-
-});
-
-
-
-
-
-
-
-
+    var appview = new AppView;
+})(jQuery);
